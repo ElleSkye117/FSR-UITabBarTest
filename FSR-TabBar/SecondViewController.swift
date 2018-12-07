7b5bdc0b02d5fb7eeb0b271a8444189e930eca0f
Michael.Leber <michael.leber@nutrien.com>
2018-12-07 13:34:07 -0700

    Initial Commit

---
+++ FSR-TabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FSR-TabBar
+//
+//  Created by MLeber on 12/7/18.
+//  Copyright © 2018 MLeber. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FSR-TabBarTests/FSR_TabBarTests.swift
@@ -0,0 +1,34 @@
+//
+//  FSR_TabBarTests.swift
+//  FSR-TabBarTests
+//
+//  Created by MLeber on 12/7/18.
+//  Copyright © 2018 MLeber. All rights reserved.
+//
+
+import XCTest
+@testable import FSR_TabBar
+
+class FSR_TabBarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
